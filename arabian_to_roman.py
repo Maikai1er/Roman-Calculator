28740829525c1190830c3c4926ee0c17060ab5ea
Maikai1er <kolesnichenko.misha12@gmail.com>
2024-03-24 22:17:48 +0100

    Everything works with explanations!

--- arabian_to_roman.py
+++ arabian_to_roman.py
@@ -8,9 +8,6 @@ reversed_symbols = {
     1: 'I'
 }
 
-test_num = 419
-result = []
-
 
 def split_to_divisions(num):
     digits = [int(d) for d in str(num)]
@@ -28,28 +25,25 @@ def arabian_to_roman(num):
     result_list = []
     for digit in digits:
         if str(digit).startswith('4'):
-            substracting_arab = digit / 4
-            prime_arab = digit / 4 * 5
-            substracting_roman = reversed_symbols[int(substracting_arab)]
+            substracting_arabian = digit / 4
+            prime_arabian = digit / 4 * 5
+            substracting_roman = reversed_symbols[int(substracting_arabian)]
             result_list.append(substracting_roman)
-            prime_roman = reversed_symbols[int(prime_arab)]
+            prime_roman = reversed_symbols[int(prime_arabian)]
             result_list.append(prime_roman)
         elif str(digit).startswith('9'):
-            substracting_arab = digit / 9
-            prime_arab = digit / 9 * 10
-            substracting_roman = reversed_symbols[int(substracting_arab)]
+            substracting_arabian = digit / 9
+            prime_arabian = digit / 9 * 10
+            substracting_roman = reversed_symbols[int(substracting_arabian)]
             result_list.append(substracting_roman)
-            prime_roman = reversed_symbols[int(prime_arab)]
+            prime_roman = reversed_symbols[int(prime_arabian)]
             result_list.append(prime_roman)
         else:
             for value, symbol in reversed_symbols.items():
                 while digit >= value:
-                    print(digit, value, symbol)
                     result_list.append(symbol)
-                    print(result_list)
                     digit -= value
                     if digit == 0:
                         break
-    print(result_list)
     return ''.join(result_list)
 
--- main.py
+++ main.py
@@ -1,35 +1,50 @@
-symbols = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-}
+import time
+
+from roman_to_arabian import roman_to_arabian
+from arabian_to_roman import arabian_to_roman
 
 
-# to start with, just refactor roman num to arabian
+def calculate_roman():
+    print('Welcome to a simple calculator. Please read the instructions below.')
+    time.sleep(1)
+    print('Keep in mind that you have to enter two capitalized roman numbers separated by a sign')
+    time.sleep(1)
+    input_roman = input('Enter your expression: ')
+    listed_expression = input_roman.split(' ')
+    first_number = listed_expression[0]
+    second_number = listed_expression[2]
+    expression_sign = listed_expression[1]
+    calculated_result = None
+    match expression_sign:
+        case '+':
+            calculated_result = roman_to_arabian(first_number) + roman_to_arabian(second_number)
+        case '-':
+            calculated_result = roman_to_arabian(first_number) - roman_to_arabian(second_number)
+        case '*':
+            calculated_result = roman_to_arabian(first_number) * roman_to_arabian(second_number)
+        case '/':
+            calculated_result = round(roman_to_arabian(first_number) / roman_to_arabian(second_number))
+        case _:
+            calculated_result = 'Invalid sign, please try again.'
+    return calculated_result
 
 
-input_number = str(input('Enter a roman number: '))
+math_result = calculate_roman()
 
 
-def convert_roman_to_arabian(num):
-    list_num = list(num)
-    temp_symbol = symbols[list_num[-1]]
-    current_sum = 0
-    for symbol in reversed(list_num):
-        if temp_symbol > symbols[symbol]:
-            current_sum -= symbols[symbol]
-        else:
-            current_sum += symbols[symbol]
-        temp_symbol = symbols[symbol]
-    return current_sum
-
-# def convert_arabian_to_roman(num):
+def choose_output_style():
+    time.sleep(1)
+    print('Which output style to you prefer?')
+    time.sleep(1)
+    style = input('Please, choose one of the following options: roman or arabian and print it below: ')
+    output = None
+    if style == 'arabian':
+        output = math_result
+    if style == 'roman':
+        output = arabian_to_roman(math_result)
+    return output
 
 
-result = convert_roman_to_arabian(input_number)
-
-print(result)
+output_result = choose_output_style()
+time.sleep(1)
+print(f'The result is {output_result}')
